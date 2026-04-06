b83bfedbdbe3b8b2179b045d32b75633dfc3625c
Mrugesh Panchal <mpanchal@adobe.com>
2026-04-06 23:29:30 +0530

    updated with latst changes

--- src/converter/modules/utils/schema-org-util.js
+++ src/converter/modules/utils/schema-org-util.js
@@ -15,7 +15,6 @@ const CONTENT_TYPE_TO_SCHEMA_TYPE = {
   Troubleshooting: 'TechArticle',
   Event: 'Event',
 };
-
 const SOFTWARE_APPLICATION_TYPE = 'SoftwareApplication';
 const ADOBE_PUBLISHER = {
   '@type': 'Organization',
